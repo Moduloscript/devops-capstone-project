a12aad79fcf1d0df2f30a50e2a44422d7fed3fdc
Moduloscript <maduemeka254@gmail.com>
2026-06-30 14:41:31 +0100

    chore: gitignore temp scripts, remove tracked temp files

--- remove_temp_file.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-os.chdir(r'c:\Users\T490\Documents\modulo-vault\devops-capstone-project')
-result = subprocess.run(['git', 'rm', '--cached', '%TEMP%/check_pr.py'], capture_output=True, text=True)
-print('STDOUT:', result.stdout)
-print('STDERR:', result.stderr)
-print('RC:', result.returncode)
